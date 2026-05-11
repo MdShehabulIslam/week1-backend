578947d727ef77a6b7a76a9646ef8d1b9d592085
Mohammad Shehabul Islam <107636160+MdShehabulIslam@users.noreply.github.com>
2026-05-10 22:13:43 -0230

    Task 2 complete

---
+++ index.js
@@ -0,0 +1,19 @@
+const helpers = require("./utils/helpers.js");
+
+// Task - 2
+
+console.log(helpers.greet("Mohammad"));
+
+console.log(helpers.calculateAge(1998));
+
+console.log(helpers.formatCurrency(200));
+
+// Destructuring
+
+const { greet, calculateAge, formatCurrency } = require("./utils/helpers.js");
+
+console.log(greet("Sam"));
+
+console.log(calculateAge(2000));
+
+console.log(formatCurrency(100));
---
+++ utils/helpers.js
@@ -0,0 +1,14 @@
+function greet(name) {
+  return `Hey ${name}! Welcome to TechNest Backend Engineering.`;
+}
+
+function calculateAge(birthYear) {
+  return new Date().getFullYear() - birthYear;
+}
+
+function formatCurrency(amount) {
+  if (typeof amount !== "number") throw new Error("Amount must be a number");
+  return `R ${amount.toLocaleString("en-ZA", { minimumFractionDigits: 2 })}`;
+}
+
+module.exports = { greet, calculateAge, formatCurrency };
